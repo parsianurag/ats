aa8126e4a5292735803fd3a747e5c1c5304c8ef0
parsianurag <62787735+parsianurag@users.noreply.github.com>
2024-11-29 00:43:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -143,8 +143,8 @@ text-align: center;
 
 </style>
 <div class="footer">
-<p>&copy; Developed By <a href="https://www.linkedin.com/in/anurag-parsi-407377238"> Anurag Parsi</a></p>
-<p>&copy; and <a href="https://www.linkedin.com/in/anurag-parsi-407377238"> Anirudh Parsi</a></p>
+<p> Developed by <a href="https://www.linkedin.com/in/anurag-parsi-407377238"> Anurag Parsi</a></p>
+<p> and <a href="https://www.linkedin.com/in/anurag-parsi-407377238"> Anirudh Parsi</a></p>
 </div>
 """
 st.markdown(footer,unsafe_allow_html=True)
